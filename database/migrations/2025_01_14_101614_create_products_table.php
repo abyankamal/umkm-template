c358a93a479e0119109969d4d9f60e41b5a6150f
abyankamal <abyankamal8@gmail.com>
2025-01-16 22:04:30 +0700

    add migration

--- database/migrations/2025_01_14_101614_create_products_table.php
+++ database/migrations/2025_01_14_101614_create_products_table.php
@@ -9,11 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->integer('stock')->default(0);
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
