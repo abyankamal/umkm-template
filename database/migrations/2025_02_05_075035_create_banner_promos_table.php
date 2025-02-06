579c5c6240f66b013a872c7f54c108db12fa0931
abyankamal <abyankamal8@gmail.com>
2025-02-06 16:43:30 +0700

    add banner promo model

--- database/migrations/2025_02_05_075035_create_banner_promos_table.php
+++ database/migrations/2025_02_05_075035_create_banner_promos_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('banner_promos', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('subtitle')->nullable();
+            $table->string('image');
+            $table->string('url')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamp('start_date')->nullable();
+            $table->timestamp('end_date')->nullable();
             $table->timestamps();
         });
     }
