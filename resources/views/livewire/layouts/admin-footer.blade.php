c1fc628b789f45ed8c4cfefb01e3efc7eb5dff07
abyankamal <abyankamal8@gmail.com>
2025-02-14 17:36:48 +0700

    add footer admin

--- resources/views/livewire/layouts/admin-footer.blade.php
+++ resources/views/livewire/layouts/admin-footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="mt-auto bg-gray-800 text-white py-4">
+<footer class="bg-gray-800 text-white py-4">
     <div class="max-w-7xl mx-auto text-center">
         <p>&copy; {{ date('Y') }} UMKM Template. All rights reserved.</p>
     </div>
