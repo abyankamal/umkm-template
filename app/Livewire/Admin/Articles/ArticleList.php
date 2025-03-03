ee3602fb7be176e274d03201c1139f8233a4bd3b
abyankamal <abyankamal8@gmail.com>
2025-03-03 14:44:59 +0700

    add rule for article list

--- app/Livewire/Admin/Articles/ArticleList.php
+++ app/Livewire/Admin/Articles/ArticleList.php
@@ -4,13 +4,17 @@ namespace App\Livewire\Admin\Articles;
 
 use Livewire\Component;
 use App\Models\Article; // Ensure to import the Article model
+use Livewire\Attributes\Rule;
 
 class ArticleList extends Component
 {
-    public $articles; // To hold articles
-    public $title; // For the article title
-    public $content; // For the article content
-    public $articleId; // To track the article being edited
+    public $articles = []; // To hold articles
+    
+    #[Rule('required|string|max:255|unique:articles,title')]
+    public $title = ''; // For the article title
+    #[Rule('required|string')]
+    public $content = ''; // For the article content
+    public $articleId = null; // To track the article being edited
 
     public function mount()
     {
